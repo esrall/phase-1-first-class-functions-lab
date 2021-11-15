7194047e6104d2406fead78deae942e90ed3b142
Martha Solomon <marthsolomon@gmail.com>
2021-11-15 11:04:06 -0500

    semi-completed will return

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+const firstTwoDrivers = drivers.splice(0, 2)
+const lastTwoDrivers = drivers.splice(0,4)
+
+function returnFirstTwoDrivers () {
+    return firstTwoDrivers
+}
+
+function returnLastTwoDrivers () {
+    return lastTwoDrivers
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier (value) {
+    return function() {
+        return 5*5
+    }
+}
+ let fareDoubler = fare => fare*2
+ let fareTripler = fare => fare*3
+
+function selectDifferentDrivers(selectingtDrivers, drivers) {
+    if (selectingDrivers = returnFirstTwoDrivers) 
+    return firstTwoDrivers
+    else if (selectingDrivers = returnLastTwoDrivers)
+    return lastTwoDrivers
+}
+
+//I dont understand why the last check won't pass.
+//I plan to come back to this once I catch up in later assignments
